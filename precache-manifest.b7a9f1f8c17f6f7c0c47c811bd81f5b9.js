009703d451daaaff7e3878e7e032e3acf6f98f63
Esha32 <esha32000@gmail.com>
2020-10-16 15:36:55 -0700

    Updates

---
+++ precache-manifest.b7a9f1f8c17f6f7c0c47c811bd81f5b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d18649dd379a53d508d05dd6e394c89",
+    "url": "/What-s-App/index.html"
+  },
+  {
+    "revision": "ff185059964ed9067213",
+    "url": "/What-s-App/static/css/main.b8bf44e3.chunk.css"
+  },
+  {
+    "revision": "d8c5d593f5fbd9cbe9aa",
+    "url": "/What-s-App/static/js/2.b9e2fb0e.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/What-s-App/static/js/2.b9e2fb0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff185059964ed9067213",
+    "url": "/What-s-App/static/js/main.db66e009.chunk.js"
+  },
+  {
+    "revision": "6b8dfc205647598bc27d",
+    "url": "/What-s-App/static/js/runtime-main.ab3b973e.js"
+  }
+]);
